268285ed3c58c5517213c6712767b5f1a5ed6a67
Charlie Dauber <charlie.dauber@hotmail.com>
2023-12-18 18:49:52 +0000

    I have managed to find a fix for the information not going into a new div and also found a way to stop the repititon of the books, I have made the form a pop up but I need to make it look better

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const myLibrary = [];
-//let title = document.getElementById("bookTitle"); 
-//let author = document.getElementById("bookAuthor"); 
-//let pages = document.getElementById("bookPages");
-//let read = document.getElementById("bookRead");
-//let submit = document.getElementById("bookSubmit");
+let myLibrary = [];
+const allBooks = [];
 const form = document.getElementById("form");
 form.addEventListener("submit", function(event) { 
     event.preventDefault(); // Prevents the form from submitting and the page from reloading 
@@ -26,37 +22,32 @@ function AddToLibrary(){
     let pages = document.getElementById("bookPages");
     let read = document.getElementById("bookRead");
     let book = new Book(title.value, author.value, pages.value, read.value);
-    myLibrary.push(book); //may not be correct but its basically what i want to do, add the book to 
+    myLibrary.push(book);
+    allBooks.push(book); //this is to keep all books logged until I find a way to stop reprinting arrays from mylibrary
 }
 
 function DisplayBooks(){
-    for(i=0; i<myLibrary.length; i++){
-        let book = myLibrary[i];
-        
+        myLibrary.forEach((n)=>{
         let bookCard = document.createElement('div');
         bookCard.className = "bookCard"
-        document.querySelector('.bookDisplay').appendChild(bookCard);
-
-        document.querySelector('.bookCard').append(JSON.stringify(book['title'] + ", " + book['author'] + ", " + book['pages'] + ", " + book['read'])) 
-        
-        // The plan is to have someone enter the book, then update the library when they add the book. then have the library automatically create the div and place the details
-        // in the div to display then on the "book shelf". the current issue is that when we are adding a book, it is firstly reprinting the first item on the array and secondly
-        // printing into the next div that is being created.
-        //things to do:
-        //1) find a way to print only the most recently added book from array, maybe using pop or a new array etc
-        //2) find a way to add to a new div which the new book data gets added to and without creating excess empty divs, look into queryselectorall, seems interesting
-        console.log(book);
-        //add books to "cards" on the page
-    }
+        document.querySelector('.bookDisplay').innerHTML +=`<div class="bookCard">${n['title'] + ", " + n['author'] + ", " + n['pages'] + ", " + n['read']}</div>`;    
+        })
+        myLibrary = [];
 }
 
-function addItems(){
-    myLibrary.forEach(item => {
-        let p = document.createElement('p');
-        p.innerText = item;
-        document.querySelector('#bookShelf').append(JSON.stringify(p))  
-    });
+function AllBooks (){
+    myLibrary = allBooks;
 }
 
+function OpenForm(){
+    document.getElementById("fieldset").style.display = "flex";
+    document.getElementById("formButtonOpen").style.display = "none";
+    document.getElementById("formButtonClose").style.display = "block";
+}
 
+function CloseForm(){
+    document.getElementById("fieldset").style.display = "none";
+    document.getElementById("formButtonOpen").style.display = "block";
+    document.getElementById("formButtonClose").style.display = "none";
+}
 
